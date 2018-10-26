9da22d7dd025916bb4ff01aa3e8a1f2edd5c8d55
df <dferrerol92@elpuig.xeill.net>
2018-10-26 20:10:36 +0200

    dsjgndsjgnsd

--- src/exercici2/JocDaus.java
+++ src/exercici2/JocDaus.java
@@ -1,17 +1,70 @@
 package exercici2;
 
+import java.util.Scanner;
+
 public class JocDaus {
 
-    int dau1=0;
-    int da2=0;
-    int dau3=0;
-    
-    public void manager(){
+    int resultat1;
+    int resultat2;
+    int resultat3;
+
+    int victorias=0;
+    int intentos=0;
+
+    public void constructor(){
+
+        Dau d1 = new Dau();
+        Dau d2 = new Dau();
+        Dau d3 = new Dau();
+
+        resultat1= (int) (Math.random() * 6) + 1; // d1.resultado=.....
+        resultat2= (int) (Math.random() * 6) + 1;
+        resultat3= (int) (Math.random() * 6) + 1;
+
+        if(resultat1==resultat2 && resultat2==resultat3 && resultat1==resultat3){
+
+
+            resultat1=d1.resultado;
+            resultat2=d2.resultado;
+            resultat3=d3.resultado;
+
+            victorias++;
+        }
+
+
+        System.out.println("-------");
+        System.out.println(resultat1);
+        System.out.println(resultat2);
+        System.out.println(resultat3);
+        System.out.println("Victorias: " + victorias);
 
     }
 
 
     public void jugarPartida(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        //System.out.println("Indica lo que deseas hacer: 1) Jugar Partida  |   2) Salir ");
+        int opcion=1;
+
+        while(opcion !=2 ){
+        System.out.println("Indica lo que deseas hacer: 1) Jugar Partida  |   2) Salir ");
+        opcion = scanner.nextInt();scanner.nextLine();
+
+        switch (opcion){
+
+            case 1 : constructor(); intentos++;
+                System.out.println("Lo has intentado " + intentos + " veces");
+            break;
+
+            case 2:
+                System.out.println("Intentos: " + intentos);
+                System.out.println("Victorias: " + victorias); break;
+        }
+
+        }
+
 
     }
 
--- src/exercici2/Main.java
+++ src/exercici2/Main.java
@@ -3,7 +3,8 @@ package exercici2;
 public class Main {
     public static void main(String[] args) {
 
-
+JocDaus jocDaus = new JocDaus();
+jocDaus.jugarPartida();
 
 
 
